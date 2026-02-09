e9a452b5a66c9e1806d04dd3a6b751907e146923
Wowa3366-stack <matkovskyi36@ukr.net>
2026-02-09 20:43:42 +0200

    Зробив дз 1

---
+++ variables_01.js
@@ -0,0 +1,40 @@
+// Завдання 1
+const myAge = 13;
+console.log(myAge);
+
+// Завдання 2
+const myName = "vova";
+console.log(myName);
+
+// Завдання 3
+const isStudent = true;
+console.log(isStudent);
+
+// Завдання 4
+ const  myString  = "Найкраща помста — величезний успіх. Френк Сінатра";
+ console.log(myString);
+
+// Завдання 5
+let myNumber = 5;
+myNumber = myNumber + 10;  
+console.log(myNumber);
+
+// Завдання 6
+const myNull = null;
+console.log(myNull);
+
+// Завдання 7
+//  let userName = prompt("Введіть своє ім’я");
+//  alert(userName);
+//  console.log(userName);
+
+// Завдання 8
+// const message = "Ви дійсно хочете покинути сторінку?";
+// confirm(message);
+// const renewSub = confirm(message);
+// console.log(renewSub)
+
+// Завдання 9
+// alert("Увага! Дія може бути небезпечною.");
+// let isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
+// console.log(isConfirmed);
