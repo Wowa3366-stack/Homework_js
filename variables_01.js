9542473bf57230c96ce5946619b0b53e6f4d02f7
Wowa3366-stack <matkovskyi36@ukr.net>
2026-02-09 20:45:20 +0200

    Update variables_01.js

--- variables_01.js
+++ variables_01.js
@@ -11,12 +11,12 @@ const isStudent = true;
 console.log(isStudent);
 
 // Завдання 4
- const  myString  = "Найкраща помста — величезний успіх. Френк Сінатра";
- console.log(myString);
+const myString = "Найкраща помста — величезний успіх. Френк Сінатра";
+console.log(myString);
 
 // Завдання 5
 let myNumber = 5;
-myNumber = myNumber + 10;  
+myNumber = myNumber + 10;
 console.log(myNumber);
 
 // Завдання 6
@@ -24,17 +24,17 @@ const myNull = null;
 console.log(myNull);
 
 // Завдання 7
-//  let userName = prompt("Введіть своє ім’я");
-//  alert(userName);
-//  console.log(userName);
+let userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(userName);
 
 // Завдання 8
-// const message = "Ви дійсно хочете покинути сторінку?";
-// confirm(message);
-// const renewSub = confirm(message);
-// console.log(renewSub)
+const message = "Ви дійсно хочете покинути сторінку?";
+confirm(message);
+const renewSub = confirm(message);
+console.log(renewSub);
 
 // Завдання 9
-// alert("Увага! Дія може бути небезпечною.");
-// let isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
-// console.log(isConfirmed);
+alert("Увага! Дія може бути небезпечною.");
+let isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
+console.log(isConfirmed);
