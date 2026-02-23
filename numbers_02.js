8b76f74eae3bc45d7bf2d0a9b16a8fe443685824
Wowa3366-stack <matkovskyi36@ukr.net>
2026-02-23 23:49:46 +0200

    Зробив дз 2

---
+++ numbers_02.js
@@ -0,0 +1,53 @@
+// 1
+const celsius = 25;
+const fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit);
+
+// 2
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+// 3
+const health = 100;
+const energy = 80;
+const newHealth = health - 20;
+const newEnergy = energy - 15;
+console.log(newHealth);
+console.log(newEnergy);
+
+// 4
+const totalPrice = 1000;
+const discount = 0.10;
+const discountedPrice = totalPrice * (1 - discount);
+console.log(discountedPrice);
+
+// 5
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+// 6
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+// 7
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+// 8
+const number = 64;
+const sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+
+// 9
+const integer = 42;
+const stringNumber = "256";
+const convertedInt = parseInt(stringNumber);
+console.log(convertedInt);
+const convertedString = integer.toString();
+console.log(convertedString);
